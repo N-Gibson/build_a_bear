804136ecde1e1f31e1cc56903bd6b938e16fd283
Noah Gibson <noah.gibson99@gmail.com>
2020-05-06 13:55:09 -0600

    Add class and selector function to main js

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 const outfitCollection = [];
 
 function $(selector) {
-  let selectorType = selector.split('').shift(); // grab the # or . from the selector
-  let selectorName = selector.substr(1); // grab the ID or class name from the selector
-  let DOMElement = selectDOMElement(); // select the DOM element
-  function selectDOMElement() {
-    if (selectorType === '#') {
-      return document.getElementById(selectorName);
-    } else {
-      return document.querySelectorAll(selectorName);
-    }
-  }
-  return {
-    DOMElement
-  }
+  let query = document.querySelector(selector);
+
 }
