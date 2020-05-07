34d7b76d5bf2e451dadb106a05d0be8f347a81cd
Noah Gibson <noah.gibson99@gmail.com>
2020-05-07 16:35:19 -0600

    Toggle bear attributes on click

--- main.js
+++ main.js
@@ -4,16 +4,33 @@ const main = document.querySelector('main');
 
 main.addEventListener('click', () => {
   if (event.target.id) {
-    $(event.target.id);
+    $(event.target.id, event.target);
   } else if (event.target.class) {
-    $(event.target.class);
+    $(event.target.class, event.target);
   } else {
     return;
   }
 });
 
-function $(selector) {
+function $(selector, element) {
   const bearContainer = document.querySelector('.bear_container');
+  let img;
 
-  bearContainer.insertAdjacentHTML('beforeend', `<img id=${selector}${"_identifier"} src=assets/${selector}${".png"}>`);  
+  if ( element.dataset.active === 'false' ) {
+    element.dataset.active = 'true';
+
+    bearContainer.insertAdjacentHTML('beforeend', `<img id=${selector}${"_identifier"} src=assets/${selector}${".png"}>`); 
+  } else {
+    element.dataset.active = 'false'
+
+    const targetElement = `${element.id}${'_identifier'}`;
+
+    event.target.parentElement.parentElement.parentElement.childNodes[3].childNodes[1].childNodes.forEach(node => {
+      if ( node.id === targetElement ) {
+        img = node
+      }
+    })
+
+    img.remove();
+  } 
 }
