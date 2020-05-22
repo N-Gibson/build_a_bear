15bd12c499582f7de7bbe1b4e5cb0e4214dcb3bd
Noah Gibson <noah.gibson99@gmail.com>
2020-05-22 15:21:37 -0600

    Remove methods from outfits class

--- outfits.js
+++ outfits.js
@@ -1,18 +1,8 @@
 class Outfits {
   constructor(title, garments = []) {
-    this.id = null,
+    this.id = Date.now(),
     this.title = title,
     this.garments = garments,
     this.background = ""
-  }
-
-  addGarment(garment) {
-    this.garments.push(garment)
-  }
-
-  removeGarment(garment) {
-    const garmentFilter = this.garments.filter(garments => garments !== garment);
-
-    this.garments = garmentFilter;
   }
 }
